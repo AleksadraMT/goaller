08a3477836da4917c8a8ebfdc19392c948600358
aleksandramt <lawrhill.ml@gmail.com>
2019-02-11 15:33:53 +0200

    Updates

---
+++ precache-manifest.8510f0fcb89b9c641d687f5b4fc16f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goaller/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1c24e681c4dbe69f6f55",
+    "url": "/goaller/static/js/runtime~main.1c24e681.js"
+  },
+  {
+    "revision": "fe266afca5c50861e8f0",
+    "url": "/goaller/static/js/main.fe266afc.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/goaller/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "fe266afca5c50861e8f0",
+    "url": "/goaller/static/css/main.a32777b4.chunk.css"
+  },
+  {
+    "revision": "2d4b2bdcb4e8acdf6627246f9d94ff53",
+    "url": "/goaller/index.html"
+  }
+];
